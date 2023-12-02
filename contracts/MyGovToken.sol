cf0b22c72f9c37e869675fb913867291fa0e86db
batuhancetin1 <batuhan.cetin@harpeon.com>
2023-12-02 18:03:18 +0300

    code completed

--- contracts/MyGovToken.sol
+++ contracts/MyGovToken.sol
@@ -13,6 +13,7 @@ contract MyGovToken is ERC20 {
     uint numOfFundedProjects = 0;
     USDStableCoin usdStableCoinContract;
     uint numberOfMembers = 0;
+    uint reservedUSD = 0;
 
     // Constructor to initialize the token with a name and symbol
     constructor(uint tokensupply, address usdStableCoinAddress) ERC20("MyGov Token", "MYGOV") {
@@ -37,8 +38,10 @@ contract MyGovToken is ERC20 {
 
     struct User {
         address userAddress;
-        uint usdStableCoinBalance;
         bool fauceted;
+        mapping(uint => address) delegation;
+        uint lockedUntil;
+        mapping(uint => uint) weight;
     }
 
     struct ProjectProposal {
@@ -49,7 +52,12 @@ contract MyGovToken is ERC20 {
         uint[] paySchedule;
         bool isFunded;
         mapping(address => bool) voters;
+        mapping(address => bool) paymentvoters;
         uint vote; 
+        uint votepayment;
+        bool[] withdrawed;
+        
+
     }
 
     Survey[] public surveys;
@@ -67,6 +75,7 @@ contract MyGovToken is ERC20 {
     function donateUSD(uint amount) public {
         require(usdStableCoinContract.balanceOf(msg.sender) >= amount, 'Insufficient amount of USD Stable Coin');
         usdStableCoinContract.transferFrom(msg.sender, address(usdStableCoinContract), amount);
+
     }
 
     function getSurveyResults(uint surveyid) public view returns(uint numtaken, uint[] memory results) {
@@ -92,6 +101,19 @@ contract MyGovToken is ERC20 {
         funded = proposals[projectid].isFunded;
         return funded;
     }
+
+    function getProjectNextPayment(uint projectid) public view returns(int next) {
+        ProjectProposal storage project = proposals[projectid];
+        for (uint i = 0; i < project.paySchedule.length; i++) 
+        {
+            if (block.timestamp < project.paySchedule[i]) {
+                next = int(project.paymentAmounts[i]);
+                break;
+            }
+        }
+        return next;
+
+    }
     
     function getProjectOwner(uint projectid) public view returns(address projectowner) {
         projectowner = proposals[projectid].projectOwner;
@@ -115,6 +137,19 @@ contract MyGovToken is ERC20 {
     function getNoOfFundedProjects() public view returns(uint numfunded) {
         numfunded = numOfFundedProjects;
         return numfunded;
+    }
+
+    function getUSDReceivedByProject (uint projectid) public view returns(uint amount) {
+        amount = 0;
+        ProjectProposal storage project = proposals[projectid];
+        for (uint i = 0; i < project.paymentAmounts.length; i++) 
+        {
+            if (project.withdrawed[i] == false) {
+                break;
+            }
+            amount += project.paymentAmounts[i];
+        }
+        return amount;
     }
 
     function getNoOfSurveys() public view returns(uint numsurveys) {
@@ -182,20 +217,105 @@ contract MyGovToken is ERC20 {
         project.paymentAmounts = paymentamounts;
         project.paySchedule = payschedule;
         project.projectOwner = msg.sender;
+        project.vote = 0;
+        project.votepayment = 0;
 
         if (balanceOf(msg.sender) == 0) {
             numberOfMembers --;
         }
-
+        for (uint i = 0; i < project.paymentAmounts.length; i++) 
+        {
+            project.withdrawed[i] = false;
+        }
         return projectid;
     }
 
-    //function reserveProjectGrant(uint projectid) public {
-    //    ProjectProposal storage project = proposals[projectid];
-    //    require(project.isFunded, "Project must be funded.");
-    //    require(msg.sender == project.projectOwner, "User is not the project owner.");
-    //    require(project.deadline < block.timestamp, "Deadline is passed.");
-    //}
-    
+    function reserveProjectGrant(uint projectid) public {
+        ProjectProposal storage project = proposals[projectid];
+        require(msg.sender == project.projectOwner, "User is not the project owner.");
+        require(project.deadline < block.timestamp, "Deadline is passed.");
+        require(project.vote >= numberOfMembers/10, "Insufficient vote");
+        uint funding = 0;
+        
+        for (uint i = 0; i < project.paymentAmounts.length; i ++) 
+        {
+            funding += project.paymentAmounts[i];
+        }
+
+        require(usdStableCoinContract.balanceOf(address(usdStableCoinContract)) >= funding + reservedUSD, "Insufficient amount of USD to grant");
+        reservedUSD += funding;
+        project.isFunded = true;
+        numOfFundedProjects ++;
+    }
+
+    function withdrawProjectPayment(uint projectid) public {
+        ProjectProposal storage project = proposals[projectid];
+        require(project.isFunded, "Project must be funded.");
+        require(msg.sender == project.projectOwner, "User is not the project owner.");
+        if (project.votepayment >= numberOfMembers/100) {
+            for (uint i = 0; i < project.paySchedule.length; i++) 
+            {
+                if (project.withdrawed[i] == false && project.paySchedule[i] < block.timestamp) {
+                require(project.paymentAmounts[i] < reservedUSD, "Insufficient reserved.");
+                usdStableCoinContract.transferFrom(address(usdStableCoinContract), msg.sender, project.paymentAmounts[i]);
+                reservedUSD -= project.paymentAmounts[i];
+                project.withdrawed[i] = true;
+                }
+            }
+        } else {
+            project.isFunded = false;
+        }
+        
+    }
+
+    function delegateVoteTo(address memberaddr,uint projectid) public {
+        ProjectProposal storage project = proposals[projectid];
+        User storage user = users[msg.sender];
+        User storage delegate = users[memberaddr];
+        require(project.voters[msg.sender] == false, "User already voted.");
+        require(project.voters[memberaddr] == false, "Delegated user already voted.");
+        require(msg.sender != memberaddr, "Self delegation is not allowed.");
+        require(balanceOf(msg.sender) > 0, "User must be a member.");
+        require(balanceOf(memberaddr) > 0, "Delegate must be a member.");
+        require(user.delegation[projectid] == address(0), "User already delegated his/her vote.");
+        require(delegate.delegation[projectid] == address(0), "Delegate already delegated his/her vote.");
+        user.delegation[projectid] = memberaddr;
+        if (delegate.weight[projectid] == 0 || delegate.weight[projectid] == 1) {
+            delegate.weight[projectid] = 2;
+        } else {
+            delegate.weight[projectid] ++;
+        }
+        user.lockedUntil = project.deadline;
+    }
+
+    function voteForProjectProposal(uint projectid,bool choice) public {
+        require(balanceOf(msg.sender) > 0, "Voter must be a member.");
+        ProjectProposal storage project = proposals[projectid];
+        User storage user = users[msg.sender];
+        require(project.voters[msg.sender] == false, "User already voted.");
+        require(user.delegation[projectid] == address(0), "User already delegated his/her vote.");
+        require(block.timestamp > user.lockedUntil, "User's vote is locked until deadline of another project.");
+        require(block.timestamp < project.deadline, "Deadline of the proposal is passed.");
+        if (choice == true) {
+            project.vote += user.weight[projectid];
+        }
+        project.voters[msg.sender] = true;
+    }
+
+    function voteForProjectPayment(uint projectid,bool choice) public {
+        require(balanceOf(msg.sender) > 0, "Voter must be a member.");
+        ProjectProposal storage project = proposals[projectid];
+        User storage user = users[msg.sender];
+        require(project.paymentvoters[msg.sender] == false, "User already voted.");
+        require(user.delegation[projectid] == address(0), "User already delegated his/her vote.");
+        require(block.timestamp > user.lockedUntil, "User's vote is locked until deadline of another project.");
+        require(block.timestamp < project.deadline, "Deadline of the proposal is passed.");
+        if (choice == true) {
+            project.votepayment += user.weight[projectid];
+        }
+        project.paymentvoters[msg.sender] = true;
+    }
+
+
 }
 
