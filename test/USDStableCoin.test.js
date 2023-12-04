b2617307fc200f9c4e44f17b700d7f46cf8c6b61
Egecan Serbester <87999176+egecans@users.noreply.github.com>
2023-12-04 21:09:28 +0300

    Comment USDStableCoin.test

--- test/USDStableCoin.test.js
+++ test/USDStableCoin.test.js
@@ -1,15 +1,20 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+// Test suite for USDStableCoin contract
 describe("USDStableCoin", function () {
+  // Test case: Deploy USDStableCoin and perform tests
   it("Should deploy USDStableCoin and perform tests", async function () {
+    // Get the contract factory for USDStableCoin
     const USDStableCoin = await ethers.getContractFactory("USDStableCoin");
+
+    // Deploy the USDStableCoin contract
     const usdStableCoin = await USDStableCoin.deploy();
 
-    // Retrieve total supply as a string
+    // Retrieve the total supply of USDStableCoin as a string
     const totalSupply = (await usdStableCoin.totalSupply()).toString();
 
-    // Perform the assertion
+    // Perform the assertion: Check if the total supply is equal to the expected value "10000"
     expect(totalSupply).to.equal("10000");
   });
 });
