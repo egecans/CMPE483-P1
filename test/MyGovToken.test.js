c67a33399899d56052e6eb837a883be658c44841
Egecan Serbester <87999176+egecans@users.noreply.github.com>
2023-12-04 21:09:04 +0300

    Comment MyGovToken.test

--- test/MyGovToken.test.js
+++ test/MyGovToken.test.js
@@ -7,18 +7,19 @@ chai.use(chaiAsPromised);
 
 const { expect } = chai;
 
+// Test suite for MyGovToken contract
 describe("MyGovToken", function () {
   let MyGovToken;
   let myGovTokenInstance;
   let account;
 
+  // Before each test case, deploy the MyGovToken contract
   beforeEach(async () => {
     [deployer, account] = await ethers.getSigners();
 
-    // Replace 'MyGovToken' with the name of your contract
     MyGovToken = await ethers.getContractFactory("MyGovToken");
 
-    // Deploy the contract
+    // Deploy the contract with initial supply and a specified USD Stable Coin address
     myGovTokenInstance = await MyGovToken.connect(deployer).deploy(
       20000000,
       "0xd9145CCE52D386f254917e481eB44e9943F39138"
@@ -26,11 +27,14 @@ describe("MyGovToken", function () {
     await myGovTokenInstance.deployed();
   });
 
+  // Test case: Verify that the contract is deployed successfully
   it("should deploy MyGovToken contract", async () => {
     expect(await myGovTokenInstance.name()).to.equal("MyGov Token");
   });
 
-  it("should transfer tokens to 100 addresses", async () => {
+  // Test case: Transfer tokens to NumOfAddresses addresses
+  const NumOfAddresses = 300;
+  it("should transfer tokens to 300 addresses", async () => {
     // Function to generate Ethereum addresses
     function generateAddresses(numAddresses) {
       const addresses = [];
@@ -43,16 +47,18 @@ describe("MyGovToken", function () {
       return addresses;
     }
 
-    const addresses = generateAddresses(100);
+    // Generate addresses and transfer tokens
+    const addresses = generateAddresses(NumOfAddresses);
     let totalGasSpent = ethers.BigNumber.from(0);
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < NumOfAddresses; i++) {
       const address = addresses[i];
 
       const tx = await myGovTokenInstance
         .connect(deployer)
         .transfer(address, ethers.utils.parseEther("0"));
 
+      // Retrieve balance, gas spent, and log results
       const balance = await myGovTokenInstance.balanceOf(address);
 
       // Wait for the transaction to be mined and get the receipt
@@ -77,6 +83,7 @@ describe("MyGovToken", function () {
     }
   });
 
+  // Test case: Donate MyGovToken
   it("should donate MyGovToken", async () => {
     // Initial balances
     const initialSenderBalance = await myGovTokenInstance.balanceOf(
@@ -100,6 +107,7 @@ describe("MyGovToken", function () {
       myGovTokenInstance.address
     );
 
+    // Log balances
     console.log("Initial Sender Balance:", initialSenderBalance.toString());
     console.log("Donation Amount:", donationAmount.toString());
     console.log("Final Sender Balance:", finalSenderBalance.toString());
@@ -113,6 +121,7 @@ describe("MyGovToken", function () {
     );
   });
 
+  // Test case: Revert if sender has insufficient balance
   it("should revert if sender has insufficient balance", async () => {
     // Attempt to donate more than the sender's balance
     const donationAmount = 10;
